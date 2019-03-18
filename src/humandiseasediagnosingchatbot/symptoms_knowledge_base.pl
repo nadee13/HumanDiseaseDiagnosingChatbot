7db2f05dbeb551ad5c670d3dee7647860125e079
nadee13 <nadeeshawi13@gmail.com>
2019-03-18 22:11:35 +0530

    commit

---
+++ src/humandiseasediagnosingchatbot/symptoms_knowledge_base.pl
@@ -0,0 +1,24 @@
+%Flu
+symptom(fever, flu).
+symptom(headache, flu).
+symptom(chills, flu).
+symptom(sneezing, flu).
+
+%Commoncold
+symptom(sneezing, common_cold).
+symptom(chills, common_cold).
+symptom(fever, common_cold).
+
+%Measles
+symptom(fever, measles).
+symptom(rash, measles).
+
+%dengue
+symptom(fever, dengue).
+symptom(headache, dengue).
+symptom(rash, dengue).
+
+%chickenpox
+symptom(fever, chickenpox).
+symptom(chills, chickenpox).
+symptom(rash, chickenpox).
